18481acfbf9d28c1e53055098f18caf7a1f9f263
Biellucc <gabriellucc02@gmail.com>
2024-08-29 16:06:28 -0300

    Comecei a mexer no componente de feedback. Falta fazer o buttonpara ir para o feedback especifico.

--- resources/views/components/usuarios/feedback/index.blade.php
+++ resources/views/components/usuarios/feedback/index.blade.php
@@ -1,3 +1,35 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+@props(['feedbacks'])
+<div class="col-md-6 col-lg-12 order-md-last">
+    <ul class="list-group mb-3">
+        <h4 class="d-flex justify-content-between align-items-center mb-3">
+            @if (Auth::user()->administrador)
+                <span class="text-primary">{{ __('Feedbacks') }}</span>
+            @else
+                <span class="text-primary">{{ __('Your feedbacks') }}</span>
+            @endif
+            <span class="badge bg-primary rounded-pill">{{ $feedbacks->count() }}</span>
+        </h4>
+        @foreach ($feedbacks as $feedback)
+            <li class="list-group-item d-flex justify-content-between lh-sm mt-1">
+                <div>
+                    <h6 class="my-0">{{ $feedback->titulo }}</h6>
+                    <small class="text-body-secondary">{{ __('Email Address') }}:
+                        {{ $feedback->usuario?->email }}</small>
+                    <small class="text-body-secondary">{{ __('Created on') }}:
+                        {{ $feedback->created_at }}</small>
+                    <small class="text-body-secondary">{{ __('Updated on') }}:
+                        {{ $feedback->updated_at }}</small>
+
+                    <div class="col-4">
+                        <form action="{{}}" method="GET">
+                            <x-text-input id="feedback_id" class="block mt-1 w-full" type="hidden" name="feedback_id"
+                                :value="$feedback->id" />
+                            <x-primary-button>{{ __('Open') }}</x-primary-button>
+                        </form>
+                    </div>
+                </div>
+                <span class="text-body-secondary">{{ __('Status') }}: {{ $feedback->status }}</span>
+            </li>
+        @endforeach
+    </ul>
 </div>
--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -1,23 +1,25 @@
 <x-app-layout>
-    <div class="row mt-4">
-        <div class="col-3">
-            <form action="{{ route('cartao.formulario') }}" method="get">
-                <x-primary-button class="ms-3">
-                    {{ __('Add') }}
-                </x-primary-button>
-            </form>
-        </div>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Feedbacks') }}
+        </h2>
+    </x-slot>
 
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Feedbacks') }}
-            </h2>
-        </x-slot>
-    </div>
+    @if (!Auth::user()->administrador)
+        <div class="row mt-4">
+            <div class="col-3">
+                <form action="{{ route('cartao.formulario') }}" method="get">
+                    <x-primary-button class="ms-3">
+                        {{ __('Add') }}
+                    </x-primary-button>
+                </form>
+            </div>
+        </div>
+    @endif
 
     <div class="container mt-3">
         @if ($feedbacks->isNotEmpty())
-        <x-usuarios.feedback.index></x-usuarios.feedback.index>
+            <x-usuarios.feedback.index :feedbacks="$feedbacks"></x-usuarios.feedback.index>
         @else
             <x-h1 class="mt-5">{{ __('No feedbacks found') }}</x-h1>
         @endif
