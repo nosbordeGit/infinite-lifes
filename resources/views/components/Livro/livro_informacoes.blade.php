5eef1eb077c4299f1a39abce72e176b9f64f270e
Biellucc <gabriellucc02@gmail.com>
2024-09-23 15:48:17 -0300

    Separei a parte dos botoes da pagina do livro, assim se eu precisar reutilizar aquela página, é só chamar e alterar os botões.

--- resources/views/components/Livro/livro_informacoes.blade.php
+++ resources/views/components/Livro/livro_informacoes.blade.php
@@ -8,76 +8,7 @@
             </div>
 
             <div class="row mt-2">
-                <div class="col-4">
-                    @php
-                        if (Auth::check() && Auth::user()->cliente) {
-                            $cliente = Auth::user()->cliente;
-                            $carrinho = $cliente->carrinhos->firstWhere('status', 1);
-                            if ($carrinho != null) {
-                                $temNoCarrinho = $carrinho
-                                    ->livros()
-                                    ->where('livro_id', $livro->id)
-                                    ->first();
-                            } else {
-                                $temNoCarrinho = null;
-                            }
-
-                            $favorito = $cliente->favoritos->firstWhere('livro_id', $livro->id);
-                            //dd($favorito);
-                        } else {
-                            $temNoCarrinho = null;
-                            $favorito = null;
-                        }
-                    @endphp
-
-                    @if ($temNoCarrinho == null)
-                        @if ($livro->estoque > 0)
-                            <form action="{{ route('carrinho.adicionar') }}" method="post">
-                                @csrf
-                                <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
-                                    :value="$livro->id" />
-                                <x-primary-button>{{ __('Add') }}</x-primary-button>
-                            </form>
-                        @else
-                            <x-primary-button disabled>{{ __('Sold out') }}</x-primary-button>
-                        @endif
-                    @else
-                        <form action="{{ route('carrinho.remover') }}" method="GET">
-                            @csrf
-                            <x-text-input id="carrinho_id" class="block mt-1 w-full" type="hidden" name="carrinho_id"
-                                :value="$carrinho->id" />
-                            <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
-                                :value="$livro->id" />
-                            <x-danger-button>{{ __('Remove') }}</x-danger-button>
-                        </form>
-                    @endif
-                </div>
-                <div class="col-4">
-                    @if ($favorito == null)
-                        <form action="{{ route('favorito.adicionar') }}" method="POST">
-                            @csrf
-                            <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
-                                :value="$livro->id" />
-                            <x-primary-button>{{ __('Favorite') }}</x-primary-button>
-                        </form>
-                    @else
-                        <form action="{{ route('favorito.remover') }}" method="GET">
-                            <x-text-input id="favorito_id" class="block mt-1 w-full" type="hidden" name="favorito_id"
-                                :value="$favorito->id" />
-                            <x-danger-button>{{ __('Unfavorite') }}</x-danger-button>
-                        </form>
-                    @endif
-                </div>
-                <div class="col-4">
-                    <form action="{{ route('pedido.formulario') }}" method="GET">
-                        @csrf
-                        <x-text-input id="tipo_id" class="block mt-1 w-full" type="hidden" name="tipo_id"
-                            :value="'livro'" />
-                        <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
-                            :value="$livro->id" />
-                        <x-primary-button>{{ __('Order') }}</x-primary-button>
-                    </form>
-                </div>
+                {{ $botoes }}
             </div>
         </div>
 
--- resources/views/site/livro.blade.php
+++ resources/views/site/livro.blade.php
@@ -1,6 +1,79 @@
 <x-app-layout>
     <div class="container">
-        <x-livro.livro_informacoes :livro="$livro"></x-livro.livro_informacoes>
+        <x-livro.livro_informacoes :livro="$livro">
+            <x-slot name="botoes">
+                <div class="col-4">
+                    @php
+                        if (Auth::check() && Auth::user()->cliente) {
+                            $cliente = Auth::user()->cliente;
+                            $carrinho = $cliente->carrinhos->firstWhere('status', 1);
+                            if ($carrinho != null) {
+                                $temNoCarrinho = $carrinho
+                                    ->livros()
+                                    ->where('livro_id', $livro->id)
+                                    ->first();
+                            } else {
+                                $temNoCarrinho = null;
+                            }
+
+                            $favorito = $cliente->favoritos->firstWhere('livro_id', $livro->id);
+                            //dd($favorito);
+                        } else {
+                            $temNoCarrinho = null;
+                            $favorito = null;
+                        }
+                    @endphp
+
+                    @if ($temNoCarrinho == null)
+                        @if ($livro->estoque > 0)
+                            <form action="{{ route('carrinho.adicionar') }}" method="post">
+                                @csrf
+                                <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
+                                    :value="$livro->id" />
+                                <x-primary-button>{{ __('Add') }}</x-primary-button>
+                            </form>
+                        @else
+                            <x-primary-button disabled>{{ __('Sold out') }}</x-primary-button>
+                        @endif
+                    @else
+                        <form action="{{ route('carrinho.remover') }}" method="GET">
+                            @csrf
+                            <x-text-input id="carrinho_id" class="block mt-1 w-full" type="hidden" name="carrinho_id"
+                                :value="$carrinho->id" />
+                            <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
+                                :value="$livro->id" />
+                            <x-danger-button>{{ __('Remove') }}</x-danger-button>
+                        </form>
+                    @endif
+                </div>
+                <div class="col-4">
+                    @if ($favorito == null)
+                        <form action="{{ route('favorito.adicionar') }}" method="POST">
+                            @csrf
+                            <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
+                                :value="$livro->id" />
+                            <x-primary-button>{{ __('Favorite') }}</x-primary-button>
+                        </form>
+                    @else
+                        <form action="{{ route('favorito.remover') }}" method="GET">
+                            <x-text-input id="favorito_id" class="block mt-1 w-full" type="hidden" name="favorito_id"
+                                :value="$favorito->id" />
+                            <x-danger-button>{{ __('Unfavorite') }}</x-danger-button>
+                        </form>
+                    @endif
+                </div>
+                <div class="col-4">
+                    <form action="{{ route('pedido.formulario') }}" method="GET">
+                        @csrf
+                        <x-text-input id="tipo_id" class="block mt-1 w-full" type="hidden" name="tipo_id"
+                            :value="'livro'" />
+                        <x-text-input id="livro_id" class="block mt-1 w-full" type="hidden" name="livro_id"
+                            :value="$livro->id" />
+                        <x-primary-button>{{ __('Order') }}</x-primary-button>
+                    </form>
+                </div>
+            </x-slot>
+        </x-livro.livro_informacoes>
         <div class="border-bottom border-light-subtle">
             <h5><strong>{{ __('Comments') }}</strong></h5>
         </div>
